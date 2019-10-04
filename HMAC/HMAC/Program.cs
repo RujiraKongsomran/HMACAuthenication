7feaf222d4b50cac41b10a46dd6a9a548d095918
Rujira Kongsomran <biwberry@outlook.com>
2019-10-04 10:47:20 +0700

    Call PUT API

--- HMAC/HMAC/Program.cs
+++ HMAC/HMAC/Program.cs
@@ -9,6 +9,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Globalization;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace HMAC
 {
@@ -17,26 +19,79 @@ namespace HMAC
         static void Main(string[] args)
         {
             // Wait : work this function util finish
-            RunAsync().Wait();
+            //RunAsync().Wait();
+            RunAPI();
         }
 
-        static async Task RunAsync()
+        //static async Task RunAsync()
+        //{
+        //    var apiBaseAddress = new Uri("https://api.fieldclimate.com/v1");
+
+        //    CustomDelegatingHandler customDelegatingHandler = new CustomDelegatingHandler(new HttpClientHandler());
+        //    // Library request http
+        //    HttpClient client = new HttpClient(customDelegatingHandler);
+
+        //    client.BaseAddress = apiBaseAddress;
+        //    // Format response
+        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //    // GET Method
+        //    HttpResponseMessage response = await client.GetAsync("/user");
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string responseString = await response.Content.ReadAsStringAsync();
+
+        //        // Process JSON here
+        //        // Print status code and response
+        //        Console.WriteLine("Status {0}:{1}", response.StatusCode, JsonHelper.FormatJson(responseString));
+        //    }
+        //    else
+        //    {
+        //        // Handle response codes not equal to 200 here
+        //        Console.WriteLine("Status {0}:{1}", response.StatusCode, await response.Content.ReadAsStringAsync());
+        //    }
+        //}
+
+        static void RunAPI()
         {
-            var apiBaseAddress = new Uri("https://api.fieldclimate.com/v1");
 
-            CustomDelegatingHandler customDelegatingHandler = new CustomDelegatingHandler(new HttpClientHandler());
-            // Library request http
-            HttpClient client = new HttpClient(customDelegatingHandler);
+            string publicKey = "e69acf976fe887bbf522aefcc50db387bc4c20001455181d";
+            string privateKey = "3006f1fbe55d0bdbf9e407db69feed3722ce799161299f93";
+            CultureInfo enUsCulture = new CultureInfo("en-us"); // 2019
 
-            client.BaseAddress = apiBaseAddress;
-            // Format response
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            // GET Method
-            HttpResponseMessage response = await client.GetAsync("user/stations");
+            // customize header to send HMAC
+            string requestHttpMethod = "PUT";
+            string requestPath = "/user";
 
-            if (response.IsSuccessStatusCode)
+            // UTC 0
+            DateTimeOffset date = DateTimeOffset.UtcNow;
+            // set format date
+            string requestTimeStamp = date.ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'", enUsCulture);
+            string signatureRawData = String.Format("{0}{1}{2}{3}", requestHttpMethod, requestPath, requestTimeStamp, publicKey);
+            // convert privateKey to byte
+            byte[] privateKeyByteArray = Encoding.UTF8.GetBytes(privateKey);
+            // convert signatureRawData to byte
+            byte[] signature = Encoding.UTF8.GetBytes(signatureRawData);
+
+            // using : use hmac within {} only
+            string authorization = "";
+            using (HMACSHA256 hmac = new HMACSHA256(privateKeyByteArray))
             {
-                string responseString = await response.Content.ReadAsStringAsync();
+                byte[] signatureBytes = hmac.ComputeHash(signature);
+                string requestSignatureString = ByteArrayToString(signatureBytes);
+                authorization = "hmac " + string.Format("{0}:{1}", publicKey, requestSignatureString);
+            }
+
+            var client = new RestClient("https://api.fieldclimate.com/v1/user");
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader("Date", requestTimeStamp);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("Authorization", authorization);
+            request.AddParameter("application/json", "{\r\n    \"username\" :\"Kubota\",\r\n    \"info.name\" : \"Vishnu\",\r\n    \"info.phone\": \"0900000099\"\r\n}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                string responseString = response.Content;
 
                 // Process JSON here
                 // Print status code and response
@@ -45,65 +100,75 @@ namespace HMAC
             else
             {
                 // Handle response codes not equal to 200 here
-                Console.WriteLine("Status {0}:{1}", response.StatusCode, await response.Content.ReadAsStringAsync());
+                Console.WriteLine("Status {0}:{1}", response.StatusCode, response.Content);
             }
-        }
-    }
-
-    public class CustomDelegatingHandler : DelegatingHandler
-    {
-        private const string publicKey = "e69acf976fe887bbf522aefcc50db387bc4c20001455181d";
-        private const string privateKey = "3006f1fbe55d0bdbf9e407db69feed3722ce799161299f93";
-        static readonly CultureInfo enUsCulture = new CultureInfo("en-us"); // 2019
-
-        public CustomDelegatingHandler(HttpClientHandler handler)
-        {
-            base.InnerHandler = handler;
+            //Console.WriteLine("Status {0}:{1}", response.StatusCode, JsonHelper.FormatJson(response.Content));
         }
 
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            // customize header to send HMAC
-            // requestHttpMethod = GET
-            string requestHttpMethod = request.Method.Method;
-            // /user/stations
-            string requestPath = request.RequestUri.AbsolutePath;
-
-            // UTC 0
-            DateTimeOffset date = DateTimeOffset.UtcNow;
-            // put date in header
-            request.Headers.Date = date;
-            // set format date
-            string requestTimeStamp = date.ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'", enUsCulture);
-
-            string signatureRawData = String.Format("{0}{1}{2}{3}", requestHttpMethod, requestPath, requestTimeStamp, publicKey);
-            // convert privateKey to byte
-            byte[] privateKeyByteArray = Encoding.UTF8.GetBytes(privateKey);
-            // convert signatureRawData to byte
-            byte[] signature = Encoding.UTF8.GetBytes(signatureRawData);
-
-            // using : use hmac within {} only
-            using (HMACSHA256 hmac = new HMACSHA256(privateKeyByteArray))
-            {
-                byte[] signatureBytes = hmac.ComputeHash(signature);
-                string requestSignatureString = ByteArrayToString(signatureBytes);
-                request.Headers.Authorization = new AuthenticationHeaderValue("hmac", string.Format("{0}:{1}", publicKey, requestSignatureString));
-            }
-
-            //response = base.SendAsync(request, cancellationToken);
-            //return response;
-
-            return base.SendAsync(request, cancellationToken);
-        }
-
-        private string ByteArrayToString(byte[] ba)
+        private static string ByteArrayToString(byte[] ba)
         {
             StringBuilder hex = new StringBuilder(ba.Length * 2);
             foreach (byte b in ba)
                 hex.AppendFormat("{0:x2}", b);
             return hex.ToString();
         }
+
     }
+
+
+    //public class CustomDelegatingHandler : DelegatingHandler
+    //{
+    //    private const string publicKey = "e69acf976fe887bbf522aefcc50db387bc4c20001455181d";
+    //    private const string privateKey = "3006f1fbe55d0bdbf9e407db69feed3722ce799161299f93";
+    //    static readonly CultureInfo enUsCulture = new CultureInfo("en-us"); // 2019
+
+    //    public CustomDelegatingHandler(HttpClientHandler handler)
+    //    {
+    //        base.InnerHandler = handler;
+    //    }
+
+    //    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    //    {
+    //        // customize header to send HMAC
+    //        // requestHttpMethod = GET
+    //        string requestHttpMethod = request.Method.Method;
+    //        // /user/stations
+    //        string requestPath = request.RequestUri.AbsolutePath;
+
+    //        // UTC 0
+    //        DateTimeOffset date = DateTimeOffset.UtcNow;
+    //        // put date in header
+    //        request.Headers.Date = date;
+    //        // set format date
+    //        string requestTimeStamp = date.ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'", enUsCulture);
+
+    //        string signatureRawData = String.Format("{0}{1}{2}{3}", requestHttpMethod, requestPath, requestTimeStamp, publicKey);
+    //        // convert privateKey to byte
+    //        byte[] privateKeyByteArray = Encoding.UTF8.GetBytes(privateKey);
+    //        // convert signatureRawData to byte
+    //        byte[] signature = Encoding.UTF8.GetBytes(signatureRawData);
+
+    //        // using : use hmac within {} only
+    //        using (HMACSHA256 hmac = new HMACSHA256(privateKeyByteArray))
+    //        {
+    //            byte[] signatureBytes = hmac.ComputeHash(signature);
+    //            string requestSignatureString = ByteArrayToString(signatureBytes);
+    //            request.Headers.Authorization = new AuthenticationHeaderValue("hmac", string.Format("{0}:{1}", publicKey, requestSignatureString));
+    //        }
+
+    //        //response = base.SendAsync(request, cancellationToken);
+    //        //return response;
+
+    //        return base.SendAsync(request, cancellationToken);
+    //    }
+    //    private string ByteArrayToString(byte[] ba)
+    //    {
+    //        StringBuilder hex = new StringBuilder(ba.Length * 2);
+    //        foreach (byte b in ba)
+    //            hex.AppendFormat("{0:x2}", b);
+    //        return hex.ToString();
+    //    }
+    //}
 
     class JsonHelper
     {
